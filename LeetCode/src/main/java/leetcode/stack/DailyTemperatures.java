0aa03f4b9246b3afe27438b7e84c592e86e694b5
long.nguyen18 <dent.for.works@gmail.com>
2026-03-15 18:05:13 +0700

    Solution for Daily Temperatures

---
+++ LeetCode/src/main/java/leetcode/stack/DailyTemperatures.java
@@ -0,0 +1,27 @@
+package leetcode.stack;
+
+import java.sql.Array;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int currentIndex = 0; currentIndex < n; currentIndex++) {
+            while (!stack.isEmpty() && temperatures[currentIndex] > temperatures[stack.peek()]) {
+                int previousIndex = stack.pop();
+                result[previousIndex] = currentIndex - previousIndex;
+            }
+
+            stack.push(currentIndex);
+        }
+
+        return result;
+
+    }
+}
