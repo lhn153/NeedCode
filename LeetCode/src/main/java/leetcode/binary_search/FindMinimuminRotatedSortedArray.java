2a5a8a725988a524db108b5ce1e45cf5a2b8d9d0
long.nguyen18 <dent.for.works@gmail.com>
2026-03-23 00:16:47 +0700

    Solution to find min

---
+++ LeetCode/src/main/java/leetcode/binary_search/FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,19 @@
+package leetcode.binary_search;
+
+public class FindMinimuminRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length-1;
+
+        while (l < r){
+            int mid = l + (r-l)/2;
+            if (nums[mid] < nums[r]){
+                    r = mid;
+            }
+            else if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            }
+        }
+
+        return  nums[l];
+    }
+}
